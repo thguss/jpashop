bd94197ccf0eec467ab3f8ffb978b7a5ea1998d4
thguss <thgus345@naver.com>
2022-10-19 01:42:15 +0900

    [회원 도메인 개발] 회원 레포지토리 개발: 어노테이션 추가

--- src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.repository;
 
 import com.jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,9 +9,9 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
